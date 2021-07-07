e56088dfcfe83acf797b1a6279fddd5a24ff7ec9
Serguei Klebtcha <serguei.klebtcha2@ibm.com>
2021-07-08 01:25:13 +0300

    class DistanceApplicationTests set to Disabled

--- src/test/java/com/training/distance/DistanceApplicationTests.java
+++ src/test/java/com/training/distance/DistanceApplicationTests.java
@@ -1,12 +1,13 @@
 package com.training.distance;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DistanceApplicationTests {
 
     @Test
+    @Disabled
     void contextLoads() {
     }
 
